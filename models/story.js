0285a5a3c9c6af82cc3d3f3ef7e51e0f158aa9d2
Ashley <ashley.sands01@gmail.com>
2023-11-21 16:59:14 -0500

    add story and review schema

---
+++ models/story.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const reviewSchema = new Schema(
+  {
+    rating: { type: Number, enum: [1, 2, 3, 4, 5] },
+    content: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const storySchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: { type: String, enum: [] },
+    warning: {
+      type: String,
+      enum: ['Extreme Violence', 'Trauma'],
+    },
+    rating: { type: String, enum: ['General', 'Teen', 'Mature', 'Explicit'] },
+    summary: { type: String },
+    reviews: [reviewSchema],
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Story', storySchema)
