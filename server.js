590ebc523fc77cdabf1f11b04b81e9a269477225
Ashley <ashley.sands01@gmail.com>
2023-11-21 15:55:46 -0500

    add public folder

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const logger = require('morgan')
 const session = require('express-session')
 const passport = require('passport')
@@ -14,9 +15,13 @@ const exampleRouter = require('./routes/example')
 
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(
   session({
