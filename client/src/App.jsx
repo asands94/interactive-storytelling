247d12c2631d10ed7c4f7926179ca2061a19dd54
Ashley <ashley.sands01@gmail.com>
2023-12-02 19:44:14 -0500

    add createContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import Header from './components/header/Header'
 import Main from './components/Main'
+import { StoryContext } from './data/StoryContext'
 
 function App() {
+  const { Provider: StoryData, Consumer } = StoryContext
+  const [state, setState] = useState({})
+
   return (
     <>
-      <Header />
-      <Main />
+      <StoryData value={{ state, setState }}>
+        <Header />
+        <Main />
+      </StoryData>
     </>
   )
 }
---
+++ client/src/data/StoryContext.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react'
+const StoryContext = createContext()
+
+export { StoryContext }
