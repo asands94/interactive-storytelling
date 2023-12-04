f8b3d541d91c49f9d3a128a51ae3076a217c521e
Ashley <ashley.sands01@gmail.com>
2023-12-03 21:35:19 -0500

    set up routes to connect to frontend

--- routes/index.js
+++ routes/index.js
@@ -5,37 +5,27 @@ const passport = require('passport')
 
 router.get(
   '/auth/google',
-  passport.authenticate(
-    // Which passport strategy is being used?
-    'google',
-    {
-      // Requesting the user's profile and email
-      scope: ['profile', 'email'],
-      // Optionally force user to pick account every time
-      prompt: 'select_account',
-    }
-  )
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    prompt: 'select_account',
+  })
 )
 
 router.get(
-  '/oauth2callback',
-  passport.authenticate('google', {
-    failureRedirect: '/',
-  }),
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
     if (!req.user.username) {
-      // Redirect to edit profile page for new users
-      res.redirect(process.env.NEW_USER_REDIRECT)
+      res.redirect(`${process.env.NEW_USER_REDIRECT}/${req.user._id}/edit`)
     } else {
-      // Redirect to main page for existing user
-      res.redirect(process.env.LOGOUT_EXISTING_USER_REDIRECT)
+      res.redirect(`${process.env.USER_REDIRECT}`)
     }
   }
 )
 
 router.get('/logout', function (req, res) {
   req.logout(function () {
-    res.redirect(process.env.LOGOUT_EXISTING_USER_REDIRECT)
+    res.redirect(process.env.LOGOUT_REDIRECT)
   })
 })
 
